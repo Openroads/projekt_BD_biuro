d27d3955403937d923491221d5f11cba154f1cda
daren <daren22@o2.pl>
2016-06-09 16:56:36 +0200

    Zaczety skrypcik sql

---
+++ biuro_podrozy.sql
@@ -0,0 +1,42 @@
+-- Projekt Bazy danych --
+
+-- Dariusz Szyszlak		--
+-- MAriusz Grochowski	--
+-- Szymon Matysik 		--
+-- Tomasz Mazur			--
+
+-- Baza danych--
+-- Biuro podróży--
+
+
+CREATE DATABASE IF NOT EXIST 'biuro_podrozy' DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+use 'biuro_podrozy';
+
+
+-- Struktura tabeli  KLIENT --
+
+CREATE TABLE klient (
+	pesel int(11) PRIMARY KEY,
+	imie varchar(20) NOT NULL,
+	nazwisko varchar(20) NOT NULL,
+	adres varchar(25) NOT NULL
+
+);
+
+-- Struktura tabeli  TERMIN --
+
+CREATE TABLE termin(
+	dataWyjazdu date NOT NULL,
+	dataPowrotu date NOT NULL,
+	miejsce varchar(15) NOT NULL,
+	cena NOT NULL,
+	numerOferty NOT NULL,
+
+	PRIMARY KEY (dataWyjazdu,numerOferty),
+	CONSTRAINT fk_oferta_nrOferty foreign key(numerOferty) references oferta(nrOferty)
+	/* cos z keyami */
+);
+
+
+
+
