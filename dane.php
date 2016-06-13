547205847a278903dfada786bc2c0c03902d8b34
daren <daren22@o2.pl>
2016-06-13 13:30:45 +0200

    rozpoczecie rezerwacji

---
+++ dane.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>Wprowadź dane</title>
+</head>
+<body>
+
+	<form action="zarezerwuj.php" method="post">
+	<h3> Wprowadz swoje dane						</h3>
+	Pesel 		<input type="text" name="Pesel" 	size=30>	</br>
+	Imie 		<input type="text" name="Imie" 		size=30>	</br>
+	Nazwisko	<input type="text" name="Nazwisko" 	size=30>	</br>
+	Adres 		<input type="text" name="Adres" 	size=30>	</br>
+	<input type="submit" name="dod" value="Dalej">	</br>
+	
+	</form>
+
+
+</body>
+</html>
--- zarezerwuj.php
+++ zarezerwuj.php
@@ -1,20 +1,47 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>Oferty wycieczek</title>
+</head>
+<body>
+
 <?php
 	require_once "danebazy.php";
 
 	$connect = new mysqli($server_adress,$db_user,$db_password,$db_name);
-
+	$connect->query('SET NAMES utf8');
 	if($connect->errno)
 	{
-		echo $polaczenie->connect_errno;
+		echo $connect->connect_errno;
 	}
 	else{
-		 
-		
 
+		$pesel = $_POST['Pesel'];
+		/*$imie =  $_POST['Imie'];
+		$nazwisko = $_POST['Nazwisko'];
+		$adres = $POST['Adres'];*/
 
+		$pesel_query ="SELECT * from klient where pesel='$pesel'";
+		$result = $connect->query($pesel_query);
 
-		$polaczenie->close();
+		if($result!=false)
+		{
+			if($result->num_rows > 0)
+			{
+				$klient = $result->fetch_array();
+				echo $klient['imie'].$klient['nazwisko'];
+			}else{
+
+				$insert_client ="INSERT INTO klient VALUES ('{$_POST["Pesel"]}','{$_POST["Imie"]}','{$_POST["Nazwisko"]}','{$_POST["Adres"]}')";
+				$connect->query($insert_client);
+			}
+			$result->free();
+		}
+		$connect->close();
 	}
-
-
 ?>
+
+</body>
+</html>
