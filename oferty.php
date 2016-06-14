a870d9f360fc86f54f1650210c93d73d7ccd147e
Painth <painthing38@gmail.com>
2016-06-14 21:13:24 +0200

    Layout update
    Ofeertyy

--- oferty.php
+++ oferty.php
@@ -1,11 +1,34 @@
-<!DOCTYPE HTML>
-<html lang="pl">
+<link href="style.css" rel="stylesheet" type="text/css">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-		<title>Oferty wycieczek</title>
+    <title>Through The Woods - Free Alpha Studio Web Template</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="en" />
+    <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
+
 <body>
+    <div id="main">
+        <div id="header">
+            <h1>Insane Travel</h1>
+        </div>
+        <div id="menu">
+          <ul>
+                <li><a href="#">Strona GŁÓwna</a></li>
+                <li><a href="#">O NAS</a></li>
+                <li><a href="http://localhost/projekt_BD_biuro/oferty.php">OFERTY</a></li>
+                <li><a href="http://localhost/projekt_BD_biuro/terminy.php">TERMINY</a></li>
+                <li><a href="http://localhost/projekt_BD_biuro/dane.php">ZAREZERWUJ WYJAZD</a></li>
+            <li><a href="#">KONTAKT</a></li>
+            </ul>
+        </div>
+        <div id="new">
+        <center>
+          
 <?php
 	require_once "danebazy.php";
 
@@ -44,6 +67,12 @@
 
 
 ?>
-
+</center>
+<div class="clearing">&nbsp;</div>
+      </div>
+        <div id="footer">
+            <p>Copyright &copy; GMMSZ, designed by <a href="http://www.painthfolio.wordpress.com" target="_blank">PAINTHING</a></p>
+        </div>
+    </div>
 </body>
 </html>
